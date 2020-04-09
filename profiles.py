9203b3e2a5e4ce0280003a3ea19ef2a2e871435c
Sean Peters <Sean.Peters@target.com>
2020-04-08 22:03:00 -0500

    add config to distinguish when to use builds

--- profiles.py
+++ profiles.py
@@ -2,6 +2,9 @@ import os
 import argparse
 import yaml
 
+with open("config.yml", "r") as ymlfile:
+    config = yaml.load(ymlfile)
+
 fightExpressions = {
     "pw": 'fight_style="Patchwerk"',
     "lm": 'fight_style="LightMovement"',
@@ -33,40 +36,42 @@ def clear_out_folders(path):
             print(e)
 
 
-def build_settings(talent_string, profile_name, weights):
+def build_settings(talent_string, profile_name_string, weights):
     settings_string = '\n'
-    settings_string += "talents={0}\n".format(talent_string)
+    if talent_string:
+        settings_string += "talents={0}\n".format(talent_string)
     for expression in fightExpressions:
-        if expression in profile_name:
+        if expression in profile_name_string:
             settings_string += fightExpressions[expression] + "\n"
     if weights:
         settings_string += fightExpressions['weights']
     return settings_string
 
 
-with open("config.yml", "r") as ymlfile:
-    config = yaml.load(ymlfile)
-
-parser = argparse.ArgumentParser(description='Generates sim profiles.')
-parser.add_argument('dir', help='Directory to generate profiles for.')
-parser.add_argument('--weights', help='Run sims with weights', action='store_true')
-parser.add_argument('--dungeons', help='Run a dungeonsimming batch of sims.', action='store_true')
-parser.add_argument('--talents', help='indicate talent build for output.', choices=config["builds"].keys())
-parser.add_argument('--ptr', help='indicate if the sim should use ptr data.', action='store_true')
-args = parser.parse_args()
-
-clear_out_folders('%sresults/' % args.dir)
-clear_out_folders('%sprofiles/' % args.dir)
-
-# build combination list e.g. pw_sa_1
-fightStyles = ["pw", "lm", "hm"]
-addTypes = ["sa", "ba", "na"]
-targets = ["1", "2"]
-combinations = [
-    "{0}_{1}_{2}".format(fight, add, tar) for fight in fightStyles for add in addTypes for tar in targets
-]
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generates sim profiles.')
+    parser.add_argument('dir', help='Directory to generate profiles for.')
+    parser.add_argument('--weights', help='Run sims with weights', action='store_true')
+    parser.add_argument('--dungeons', help='Run a dungeonsimming batch of sims.', action='store_true')
+    parser.add_argument('--talents', help='indicate talent build for output.', choices=config["builds"].keys())
+    parser.add_argument('--ptr', help='indicate if the sim should use ptr data.', action='store_true')
+    args = parser.parse_args()
+
+    # check if sim dir requires talents
+    if not args.talents and config["sims"][args.dir[:-1]]["builds"]:
+        print("ERROR: Must provide talents for {0}/ sims.".format(args.dir[:-1]))
+        exit()
+
+    clear_out_folders('%sresults/' % args.dir)
+    clear_out_folders('%sprofiles/' % args.dir)
+
+    # build combination list e.g. pw_sa_1
+    fightStyles = ["pw", "lm", "hm"]
+    addTypes = ["sa", "ba", "na"]
+    targets = ["1", "2"]
+    combinations = [
+        "{0}_{1}_{2}".format(fight, add, tar) for fight in fightStyles for add in addTypes for tar in targets
+    ]
     for simFile in config["sims"][args.dir[:-1]]["files"]:
         baseFile = "{0}{1}".format(args.dir, simFile)
         with open(baseFile, 'r') as f:
@@ -77,11 +82,13 @@ if __name__ == '__main__':
         for profile in combinations:
             # prefix the profile name with the base file name
             profile_name = "{0}_{1}".format(simFile[:-5], profile)
-            if args.dungeons:
-                talents = config["builds"][args.talents]["dungeons"]
+            if args.talents:
+                if args.dungeons:
+                    talents = config["builds"][args.talents]["dungeons"]
+                else:
+                    talents = config["builds"][args.talents]["composite"]
             else:
-                talents = config["builds"][args.talents]["composite"]
-
+                talents = ''
             settings = build_settings(talents, profile, args.weights)
             simcFile = "profiles/{0}.simc".format(profile_name)
             with open(args.dir + simcFile, "w+") as file:
