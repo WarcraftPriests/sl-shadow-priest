7756c35f2ce644ea3c90986f15c5b450b6665c41
Moritz Lutz <moritz.lutz@gmail.com>
2020-10-08 13:46:13 +0200

    added iterations, and simcVersion to simc

--- internal/simc.py
+++ internal/simc.py
@@ -1,11 +1,12 @@
 import subprocess
 import os
 
-def sim_local(simc_path, profile_location, output_location):
+def sim_local(simc_path, profile_location, output_location, iterations, simc_build):
     locationList = output_location.split("/")
     output = open(output_location.replace("json", "log"), 'w')
+    
     try:
-        subprocess.check_call([simc_path, "json2={0}".format(output_location), profile_location], stdout=output, stderr=output)
+        subprocess.check_call([simc_path, "json2={0}".format(output_location), "iterations={0}".format(iterations), "simcVersion={0}".format(simc_build), profile_location], stdout=output, stderr=output)
         output.close()
         os.remove(output_location.replace("json", "log"))
     except:
@@ -13,4 +14,4 @@ def sim_local(simc_path, profile_location, output_location):
 
 
 def raidbots(simc_path, profile_location, simc_build, output_location, report_name, iterations):
-    sim_local(simc_path, profile_location, output_location)
+    sim_local(simc_path, profile_location, output_location, iterations, simc_build)
