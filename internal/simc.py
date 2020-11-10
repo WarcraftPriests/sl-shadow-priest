267d26149e18f1990a03662e13846bd8fb90c6f5
PsyKzz <matt.daemon660@gmail.com>
2020-11-10 21:40:21 +0000

    revert simc.py changes

--- internal/simc.py
+++ internal/simc.py
@@ -1,11 +1,10 @@
-import os
 import subprocess
-
+import os
 
 def sim_local(simc_path, profile_location, output_location, iterations):
     locationList = output_location.split("/")
     output = open(output_location.replace("json", "log"), 'w')
-
+    
     try:
         subprocess.check_call([simc_path, "json2={0}".format(output_location), "iterations={0}".format(iterations), profile_location], stdout=output, stderr=output)
         output.close()
@@ -21,4 +20,3 @@ def sim_local(simc_path, profile_location, output_location, iterations):
 
 def raidbots(simc_path, profile_location, simc_build, output_location, report_name, iterations):
     sim_local(simc_path, profile_location, output_location, iterations)
-
