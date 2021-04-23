16d49ce4dfccea8b6c4b0dab33ea110e46e9946d
Publik <sean.peters86@icloud.com>
2021-04-23 15:32:10 -0500

    initial changes

--- internal/weights.py
+++ internal/weights.py
@@ -1,5 +1,26 @@
 """weight dict definitions"""
 
+weights_sanctum_of_domination = {
+    'pw_ba_1': 0.0200000,
+    'pw_sa_1': 0.0600000,
+    'pw_na_1': 0.2750000,
+    'lm_ba_1': 0.0000000,
+    'lm_sa_1': 0.0050000,
+    'lm_na_1': 0.1700000,
+    'hm_ba_1': 0.0000000,
+    'hm_sa_1': 0.0000000,
+    'hm_na_1': 0.0500000,
+    'pw_ba_2': 0.0400000,
+    'pw_sa_2': 0.0400000,
+    'pw_na_2': 0.1200000,
+    'lm_ba_2': 0.0500000,
+    'lm_sa_2': 0.0800000,
+    'lm_na_2': 0.0800000,
+    'hm_ba_2': 0.0000000,
+    'hm_sa_2': 0.0000000,
+    'hm_na_2': 0.0100000,
+}
+
 weights_castle_nathria = {
     'pw_ba_1': 0.0200000,
     'pw_sa_1': 0.0600000,
@@ -34,4 +55,6 @@ def find_weights(key):
         return weights_single
     if key == 'weightsCastleNathria':
         return weights_castle_nathria
+    if key == 'weightsSanctumOfDomination':
+        return weights_sanctum_of_domination
     return None
